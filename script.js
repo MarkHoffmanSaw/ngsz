ef4e4cddf2f3da882d22d9fbae57365892b7a61d
Artur <tullers@mail.ru>
2022-01-29 15:44:45 +0300

    Added scrolling, sticky nav and revealing of sections

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+const autoparkSection = document.querySelector("#autopark");
 const nav = document.querySelector(".nav");
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 
-// Modal window
+/////////////////////////////////////////////////
+// ************** Modal window ************** //
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -34,4 +36,70 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-//
+/////////////////////////////////////////////////////
+// ************** Button scrolling ************** //
+
+btnScrollTo.addEventListener("click", function (e) {
+  // Get coordinates of the DOM element
+  const autoparkCoords = autoparkSection.getBoundingClientRect();
+
+  // For all version of the browser
+  window.scrollTo({
+    left: autoparkCoords.left + window.pageXOffset,
+    top: autoparkCoords.top + window.pageYOffset,
+    behavior: "smooth",
+  });
+
+  // For a modern browser
+  // autoparkCoords.scrollIntoView({ behavior: "smooth" });
+});
+
+//////////////////////////////////////////////////////
+// ************** Sticky navigation ************** //
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const settings = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, settings);
+
+headerObserver.observe(header);
+
+/////////////////////////////////////////////////////////////
+// ************** Reveal sections (smooth) ************** //
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+});
